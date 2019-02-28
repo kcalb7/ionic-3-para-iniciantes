e47753c84704412e7c3e79aad8278e99862a9c2d
Douglas Braga <douglasbraga_lp@hotmail.com>
2019-02-28 14:13:17 -0300

    teste de camera

--- src/pages/perfil/perfil.ts
+++ src/pages/perfil/perfil.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
 
 /**
  * Generated class for the PerfilPage page.
@@ -12,14 +13,41 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 @Component({
   selector: 'page-perfil',
   templateUrl: 'perfil.html',
+  providers: [
+    Camera
+  ]
 })
 export class PerfilPage {
+  private img;
+  private options: CameraOptions = {
+    quality: 100,
+    destinationType: this.camera.DestinationType.DATA_URL,
+    encodingType: this.camera.EncodingType.JPEG,
+    mediaType: this.camera.MediaType.PICTURE
+  }
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private camera: Camera) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad PerfilPage');
   }
 
+  getImg() {
+    console.log("pegando img");
+    return this.img;
+  }
+
+  tirarFoto() {
+    console.log("tirar foto");
+    this.camera.getPicture(this.options).then((imageData) => {
+      // imageData is either a base64 encoded string or a file URI
+      // If it's base64 (DATA_URL):
+      console.log("tirando foto");
+      this.img = 'data:image/jpeg;base64,' + imageData;
+      console.log("foto tirada");
+    }, (err) => {
+      console.log(err);
+    });
+  }
 }
